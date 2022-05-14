3ad94373c84f2895124e06960c03c0918ccc1231
HRA <Hriday Aggarwal>
2022-05-14 10:36:56 +0530

    added grid and error calculation part

--- scripts/linedetection.py
+++ scripts/linedetection.py
@@ -15,17 +15,35 @@ class Line_detection():
         self.image_sub = rospy.Subscriber('LineFollower/Video',Image, self.image_callback)
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel',Twist, queue_size=1)
         self.twist = Twist()
+
+    def draw_grid(img, grid_shape, color=(0, 255, 0), thickness=1):
+        h, w, _ = img.shape
+        rows, cols = grid_shape
+        dy, dx = h / rows, w / cols
+
+        # draw vertical lines
+        for x in np.linspace(start=dx, stop=w-dx, num=cols-1):
+            x = int(round(x))
+            cv2.line(img, (x, 0), (x, h), color=color, thickness=thickness)
+
+        # draw horizontal lines
+        for y in np.linspace(start=dy, stop=h-dy, num=rows-1):
+            y = int(round(y))
+            cv2.line(img, (0, y), (w, y), color=color, thickness=thickness)
+
         
     def image_callback(self):
         v = cv2.VideoCapture(0)
         # capture videoz
         while True:
-            cx=400
-            cy=425
+            cx=320
+            cy=90
             ret, frame1 = v.read()
-            frame=frame1[250:600, ::]
+            frame=frame1[300:480, ::]
+            Line_detection.draw_grid(frame,(2,2))
             # color space change
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            hsv=cv2.medianBlur(hsv,9)
             lower_black = np.array([0, 0, 0])
             upper_black = np.array([180, 255, 30])
             mask = cv2.inRange(hsv,lower_black, upper_black)
@@ -42,6 +60,8 @@ class Line_detection():
                 if M["m00"] != 0:
                     cX = int(M["m10"] / M["m00"])
                     cY = int(M["m01"] / M["m00"])
+                    d=0
+                    print(cX,cY)
                     if cX<cx:
                         d = math.sqrt(( cy-cY)**2 + ( cx-cX)**2)
                         print("turn left")
@@ -58,10 +78,11 @@ class Line_detection():
                         print("you r right")
                 else:
                     cX, cY = 0, 0
-                cv2.circle(result, (cX, cY), 5, (255, 255, 255), -1)
-                cv2.putText(result, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                
+                cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+                cv2.putText(frame, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
             cv2.imshow('Cropped', frame)
-            cv2.imshow('Orignal',frame1)
+            #cv2.imshow('Orignal',frame1)
             cv2.imshow('result', result)
             if cv2.waitKey(1) & 0xff == ord('q'):
                 break
