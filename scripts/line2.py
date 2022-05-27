7e9a0c3675ce288087ed59170cfce4d453759d3d
HRA <Hriday Aggarwal>
2022-05-28 02:34:31 +0530

    added calculation of mid point of frame in lines 46-49

--- scripts/line2.py
+++ scripts/line2.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import cv_bridge
 from sensor_msgs.msg import Image, CameraInfo
-#from std_msgs.msg import Float32
+from std_msgs.msg import Float32
 import rospy
 import math
 from robot_controller import bot_control
@@ -27,20 +27,43 @@ class Line_Follower :
         self.velocity_msg.angular.x = 0
         self.velocity_msg.angular.y = 0
         #PID control/using P controller:
-        #self.P = 0.005 #rospy.get_param("robot_controller/pid/p")
+        self.P = 0.015 #rospy.get_param("robot_controller/pid/p")
 
         rate = rospy.Rate(10)
         self.bc=bot_control()
         
-    #move function to move robot
-    # def move(self,linear,angular):
-    #     self.velocity_msg.linear.x = linear
-    #     self.velocity_msg.angular.z = angular 
-    #     self.pub.publish(self.velocity_msg)
-    
-                
+        #move function to move robot
+        # def move(self,linear,angular):
+        #     self.velocity_msg.linear.x = linear
+        #     self.velocity_msg.angular.z = angular 
+        #     self.pub.publish(self.velocity_msg)
+    def process_image(self) :
+
+        #self.cX=80
+        #self.cY=60
+        frame = self.cap
+        hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        #centre of frame 
+        (cx2,cy2)=hsv.shape[:2]
+        self.cX=cx2//2
+        self.cY=cy2//2
+        dst = cv2.medianBlur(hsv, 9)
+        lower_brg=np.array([0,0,0])
+        upper_brg=np.array([180, 255, 65])
+        mask = cv2.inRange(dst,lower_brg, upper_brg)
+        self.cropped_image1 = frame[100::, 0::]
+        #cropped_image3 = inverted_image[100::, 0::]
+        self.cropped_image3 = mask[100::, 0::]
+
+
+        
+        
     def callback(self,data):         
 
+    
+
+
+            
         self.cap = self.bridge.imgmsg_to_cv2(data, "bgr8")
 
         cv2.imshow("data" , self.cap)
@@ -49,27 +72,7 @@ class Line_Follower :
         self.process_image()
 
         self.control_loop()
-
-    
-    def process_image(self) :
-
-        self.cX=420
-        self.cY=340
-        frame = self.cap
-        hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-
-
-        dst = cv2.medianBlur(hsv, 9)
-
-
-        lower_brg=np.array([0,0,0])
-        upper_brg=np.array([180, 255, 65])
-        mask = cv2.inRange(dst,lower_brg, upper_brg)
-        self.cropped_image1 = frame[100::, 0::]
-        #cropped_image3 = inverted_image[100::, 0::]
-        self.cropped_image3 = mask[100::, 0::]
         
-    
     def control_loop(self):
             #Contours
         contours, hierarchy = cv2.findContours(self.cropped_image3 , 1, cv2.CHAIN_APPROX_NONE)
@@ -83,28 +86,27 @@ class Line_Follower :
             if M["m00"] != 0:
                 cx = int(M["m10"] / M["m00"])
                 cy = int(M["m01"] / M["m00"])
-                print("cx : "+str(cx)+"  cy : "+str(cy))
+                print("CX : "+str(cx)+"  CY : "+str(cy))
                 if cx<self.cX:
                     d = math.sqrt(( cy-self.cY)**2 + ( cx-self.cX)**2)
                     print("turn left")
                     print(d)
-                    self.bc.fix_error(0,d)
+                    self.bc.fix_error(0,2)
                 if cx>self.cX:
                     d = -(math.sqrt(( cy-self.cY)**2 + ( cx-self.cX)**2))
                     print("turn right")
                     print(d)
-                    self.bc.fix_error(0,d)
+                    self.bc.fix_error(0,2)
                 if cx==self.cX:
                     print(d)
                     self.bc.fix_error(0,0)
-                    print("you are on right path")
+                    print("you r on right path")
                 if d==0:
                     print(d)
                     self.bc.fix_error(0,0)
-                    print("you are right")
+                    print("you r right")
             else:
                 cx, cy = 0, 0
-                self.bc.fix_error(0,0)
             
             
             cv2.circle(self.cropped_image3, (cx, cy), 5, (255, 255, 255), -2)
@@ -112,22 +114,19 @@ class Line_Follower :
             #cv2.circle(cropped_image2, (cX, cY), 5, (255, 255, 255), -2)
             
             cv2.drawContours(self.cropped_image1, c, -1, (0,255,0), 1)
-        
-        #self.pub.publish(d)
             cv2.imshow("Mask",self.cropped_image3)
-        #cv2.imshow("Frame",cropped_image1)
-        #cv2.imshow("Gray",cropped_image2)
+            #cv2.imshow("Frame",cropped_image1)
+            #cv2.imshow("Gray",cropped_image2)
             cv2.waitKey(1)  
         
 def main():
     rospy.init_node("line_follower",anonymous=True)
     a=Line_Follower()
-    rospy.spin()
-    # try :
-    #     rospy.spin()
-    # except:
-    #     print("error")
 
+    try :
+        rospy.spin()
+    except:
+        print("error")
     cv2.destroyAllWindows()
 
 
--- scripts/line_follower.py
+++ scripts/line_follower.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python3
-import sys
 import cv2
 import numpy as np
 import cv_bridge
@@ -10,111 +9,88 @@ import math
 
 
 
-class Line_Follower():
+class Line_detection():
     def __init__(self):
         rospy.init_node('ERROR', anonymous=False) #initialize user default node and annoymous help in maintaining uniquness of node
         self.bridge = cv_bridge.CvBridge()
-        
         self.pub = rospy.Publisher('geometry_msgs',Float32, queue_size=10) 
         self.sub = rospy.Subscriber('geometry_msgs',Float32, queue_size=10) 
-
         rate = rospy.Rate(10)
+    def draw_grid(img, grid_shape, color=(0, 255, 0), thickness=1):
+        h, w, _ = img.shape
+        rows, cols = grid_shape
+        dy, dx = h / rows, w / cols
+
+        # draw vertical lines
+        for x in np.linspace(start=dx, stop=w-dx, num=cols-1):
+            x = int(round(x))
+            cv2.line(img, (x, 0), (x, h), color=color, thickness=thickness)
+
+        # draw horizontal lines
+        for y in np.linspace(start=dy, stop=h-dy, num=rows-1):
+            y = int(round(y))
+            cv2.line(img, (0, y), (w, y), color=color, thickness=thickness)
+
         
-        
-    def img_detection(self):
-        cap = cv2.VideoCapture(0)
-        cap.set(3, 176)
-        cap.set(4, 144)
+    def image_callback(self):
+        v = cv2.VideoCapture(0)
         # capture videoz
         while True:
-            cX=88
-            cY=72
-
-            ret, frame = cap.read()
-            hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-            
-            
-            dst = cv2.medianBlur(hsv, 9)
-            
-
- 
-            lower_brg=np.array([0,0,0])
-            upper_brg=np.array([180, 255, 65])
-            mask = cv2.inRange(dst,lower_brg, upper_brg)
-            cropped_image1 = frame[100::, 0::]
-            #cropped_image3 = inverted_image[100::, 0::]
-            cropped_image3 = mask[100::, 0::]
-            #horizontal
-            cv2.line(cropped_image1, (88, 0), (88, 144), color=(0, 255, 0), thickness=1)
-            #vertical
-            cv2.line(cropped_image1, (0, 22), (176, 22), color=(0, 255, 0), thickness=1)
-            '''
-            N = cv2.moments(brg)
-            if N["m00"] != 0:
-                cX = int(N["m10"] / N["m00"])
-                cY = int(N["m01"] / N["m00"])
-                print("cX : "+str(cX)+"  cY : "+str(cY))
-                '''
+            cx=320
+            cy=90
+            ret, frame1 = v.read()
+            frame=frame1[300:480, ::]
+            Line_detection.draw_grid(frame,(2,2))
+            # color space change
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            hsv=cv2.medianBlur(hsv,9)
+            lower_black = np.array([0, 0, 0])
+            upper_black = np.array([180, 255, 30])
+            mask = cv2.inRange(hsv,lower_black, upper_black)
+            result=cv2.bitwise_and(frame,frame,mask=mask)
             #Contours
-            contours, hierarchy = cv2.findContours(cropped_image3 , 1, cv2.CHAIN_APPROX_NONE)
-            if len(contours) >0:
-                
+            contours, hierarchy = cv2.findContours(mask, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
+            image_copy = mask.copy()
+            if len(contours) !=0:
+                cv2.drawContours(image=result, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=1,
+                                 lineType=cv2.LINE_AA)
+                #Centre
                 c=max(contours,key=cv2.contourArea)
-                
-                
                 M = cv2.moments(c)
                 if M["m00"] != 0:
-                    cx = int(M["m10"] / M["m00"])
-                    cy = int(M["m01"] / M["m00"])
-                    print("CX : "+str(cx)+"  CY : "+str(cy))
-                    if cx<(cX-5):
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    d=0
+                    print(cX,cY)
+                    if cX<cx:
                         d = math.sqrt(( cy-cY)**2 + ( cx-cX)**2)
+                        print("turn left")
+                        print(d)
+                    if cX>cx:
+                        d = -(math.sqrt(( cy-cY)**2 + ( cx-cX)**2))
                         print("turn right")
                         print(d)
-                        
-                    if cx>(cX+5):
-                        d = -(math.sqrt(( cy-cY)**2 + ( cx-cX)**2))
+                    if cX==cx:
                         print(d)
-                        
-                        print("turn left")
-                    if cx in range (cX-5,cX+6):
-                        print(d)
-                        
                         print("you r on right path")
                     if d==0:
                         print(d)
-                        
                         print("you r right")
-
-
                 else:
-                    cx, cy = 0, 0
+                    cX, cY = 0, 0
                 
-                
-                cv2.circle(cropped_image3, (cx, cy), 2, (255, 255, 255), -2)
-                cv2.circle(cropped_image1, (cx, cy), 2, (255, 255, 255), -2)
-                #cv2.circle(cropped_image2, (cX, cY), 5, (255, 255, 255), -2)
-                
-            cv2.drawContours(cropped_image1, c, -1, (0,255,0), 1)
-            
+                cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+                cv2.putText(frame, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
             self.pub.publish(d)
-            cv2.imshow("Mask",cropped_image3)
-            cv2.imshow("Frame",cropped_image1)
-            cv2.imshow("Frame1",frame)
-
-            #cv2.imshow("Gray",cropped_image2)   
-            
-
-        
-            if cv2.waitKey(10) & 0xff == ord('q'):
+            cv2.imshow('Cropped', frame)
+            #cv2.imshow('Orignal',frame1)
+            cv2.imshow('result', result)
+            if cv2.waitKey(1) & 0xff == ord('q'):
                 break
-        cap.release()
-
+        v.release()
         cv2.destroyAllWindows()
 
 
-
 if __name__=='__main__':
-    a=Line_Follower()
-    a.img_detection()
-    
+    linedec=Line_detection()
+    linedec.image_callback()
